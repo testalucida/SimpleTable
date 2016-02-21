13de37cf853f907e0d76a1c800e4904673508a28
testalucida <martin.kendel@rotmax.de>
2016-02-21 18:00:50 +0100

    TwoTables: TwoTablesSelectionCallback

--- TwoTables.h
+++ TwoTables.h
@@ -9,33 +9,41 @@
 #define TWOTABLES_H
 
 #include <FL/Fl_Tile.H>
-#include <SimpleTable.h>
-
-//class SimpleTable;
+#include "SimpleTable.h"
 
 namespace my {
     class TableData;
 }
 
+typedef void( *TwoTablesSelectionCallback ) ( SimpleTable *,
+Fl_Table_Copy::TableContext, int r1, int c1, int r2, int c2, void * );
+
+static const char *ID_LEFT = "LEFT";
+static const char *ID_RIGHT = "RIGHT";
+
+
 class TwoTables : public Fl_Tile {
 public:
     TwoTables( int x, int y, int w, int h, const char *pLbl = 0 );
     
-//    TwoTables( int x, int y, int w, int h, const char *pLbl, 
-//               SimpleTable *pLeft, SimpleTable *pRight );
-    
     /**one Model for both the tables*/
-    void setTableData( my::TableData *pData );
+    virtual void setTableData( my::TableData *pData );
+
+	/** register a selection callback function that will be called on each selection
+	change */
+	void setSelectionCallback( TwoTablesSelectionCallback, void * );
     
     /**Callback function when scrolling in the right table*/
-    static void onRightTableScrollStatic( char c, int scrollVal, void *pUserData ) {
+    static void onRightTableScrollStatic( char c, int scrollVal, void *pUserData
+    ) {
         ((TwoTables*)pUserData)->onRightTableScroll( c, scrollVal );
     }
     void onRightTableScroll( char c, int scrollVal );
     
     /**Callback function when changing selection in the left table*/
     static void onLeftSelectionStatic( Fl_Table_Copy::TableContext context, 
-                                int r1, int c1, int r2, int c2, void *pUserData ) 
+                                int r1, int c1, int r2, int c2, void *pUserData )
+                                
     {
         ((TwoTables*)pUserData)->onLeftSelection( context, r1, c1, r2, c2 );
     }
@@ -44,7 +52,8 @@ public:
     
     /**Callback function when changing selection in the right table*/
     static void onRightSelectionStatic( Fl_Table_Copy::TableContext context, 
-                                 int r1, int c1, int r2, int c2, void *pUserData ) 
+                                 int r1, int c1, int r2, int c2, void *pUserData
+                                 ) 
     {
         ((TwoTables*)pUserData)->onRightSelection( context, r1, c1, r2, c2 );
     }
@@ -52,24 +61,36 @@ public:
                            int r1, int c1, int r2, int c2 );
     
     /**Callback function when resizing the left table*/
-    static void onLeftResizeStatic( int x, int y, int w, int h, void *pUserData ) {
+    static void onLeftResizeStatic( int x, int y, int w, int h, void *pUserData )
+    {
         ((TwoTables*)pUserData)->onLeftResize( x, y, w, h );
     }
     void onLeftResize( int x, int y, int w, int h );
     
        /**Callback function when resizing the right table*/
-    static void onRightResizeStatic( int x, int y, int w, int h, void *pUserData ) {
+    static void onRightResizeStatic( int x, int y, int w, int h, void *pUserData
+    ) {
         ((TwoTables*)pUserData)->onRightResize( x, y, w, h );
     }
     void onRightResize( int x, int y, int w, int h );
+
+	/**causes _pLeft and _pRight to not show any more data*/
+	void clearData();
+
+	/**adds a Menu-Item to the rowheader popup menu (of the left hand table).
+	Custom menu items will be added behind SimpleTable's own
+	item "Copy Row Values" */
+	void addRowHeaderPopupItem( const char *pLabel, int shortcut, int flags,
+		PopupMenuCallback, int id, void *pUserdata );
     
     virtual ~TwoTables();
-private:
-//    void initTables( SimpleTable *, bool rowHeader, ResizeMode );
-private:
+
+protected:
     SimpleTable *_pLeft;
     SimpleTable *_pRight;
+private:
+	TwoTablesSelectionCallback _selectionCallback;
+	void *_pSelectionUserData;
 };
 
 #endif /* TWOTABLES_H */
-
--- src/TwoTables.cpp
+++ src/TwoTables.cpp
@@ -9,62 +9,47 @@
 //#include <SimpleTable.h>
 
 #include <my/TableData.h>
+	
 
 TwoTables::TwoTables( int x, int y, int w, int h, const char *pLbl ) 
 : Fl_Tile( x, y, w, h, pLbl )
+, _pLeft( NULL )
+, _pRight( NULL )
+, _selectionCallback( NULL )
+, _pSelectionUserData( NULL )
 {
     _pLeft = new SimpleTable( Fl_Tile::x(), Fl_Tile::y(), w/3, h );
+	_pLeft->setId( ID_LEFT );
     _pLeft->row_header( 1 );
     _pLeft->setAlternatingRowColor();
     _pLeft->setSelectionCallback( onLeftSelectionStatic, this );
-    _pLeft->setResizeMode( RESIZEMODE_LAST_COL );
     _pLeft->tab_cell_nav( 0 );
-    
+
     _pRight = new SimpleTable( _pLeft->x() + _pLeft->w(), _pLeft->y(),
                                w - _pLeft->w(), _pLeft->h() );
+	_pRight->setId( ID_RIGHT );
     _pRight->setAlternatingRowColor();
     _pRight->setScrollCallback( onRightTableScrollStatic, this );
     _pRight->setSelectionCallback( onRightSelectionStatic, this );
-    _pRight->setResizeMode( RESIZEMODE_ALL_COLS );
     _pRight->tab_cell_nav( 0 );
-
+    
     end();
 }
-
-//TwoTables( int x, int y, int w, int h, const char *pLbl, 
-//               SimpleTable *pLeft, SimpleTable *pRight )
-//{
-//    
-//}
-
-//void TwoTables::initTables( SimpleTable *pTbl, bool rowHeader, ResizeMode mode ) {
-//    
-//}
 
 void TwoTables::setTableData( my::TableData *pData ) {
     _pLeft->setTableData( pData );
     _pRight->setTableData( pData );
       
     _pLeft->showVScrollbar( false );
-    _pLeft->makeColumnsFit();
-  
-    //strange Bug: empty space right of last column.
-    //fix it explicitly:
-//    int W = _pLeft->w() - _pLeft->row_header_width();
-//    W -= 4; //borders
-//    int cmax = _pLeft->cols();
-//    int cw = W / cmax;
-//    int gw = 0;
-//    for( int c = 0; c < (cmax-1); c++ ) {
-//        _pLeft->col_width( c, cw );
-//        gw += cw;
-//    }
-//    int rem = W - gw;
-//    _pLeft->col_width( cmax-1, rem );
-//    _pLeft->redraw();
     
     _pRight->set_selection( -1, -1, -1, -1 );
-     _pLeft->set_selection( 0, 0, 0, 0 );
+    _pLeft->set_selection( 0, 0, 0, 0 );
+}
+
+void TwoTables::setSelectionCallback( TwoTablesSelectionCallback cb, void
+*pUserData ) {
+	_selectionCallback = cb;
+	_pSelectionUserData = pUserData;
 }
 
 void TwoTables::onRightTableScroll( char c, int scrollVal ) {
@@ -80,19 +65,33 @@ void TwoTables::onLeftSelection( Fl_Table_Copy::TableContext context,
     } else {
         _pRight->set_selection( -1, -1, -1, -1 );
     }
+
+	if( _selectionCallback ) {
+		( *_selectionCallback )( _pRight, context, -1, -1, -1, -1, _pSelectionUserData
+		);
+		( *_selectionCallback )( _pLeft, context, r1, c1, r2, c2, _pSelectionUserData
+		);		
+	}
 }
 
  void TwoTables::onRightSelection( Fl_Table_Copy::TableContext context, 
                                    int r1, int c1, int r2, int c2 ) 
  {
      _pLeft->set_selection( -1, -1, -1, -1 );
+
+	 if( _selectionCallback ) {
+		 ( *_selectionCallback )( _pLeft, context, -1, -1, -1, -1, _pSelectionUserData
+		 );
+		 ( *_selectionCallback )( _pRight, context, r1, c1, r2, c2, _pSelectionUserData
+		 );		 
+	 }
  }
  
  void TwoTables::onLeftResize( int x, int y, int w, int h ) {
      //_pLeft->makeColumnsFit();
 //    fprintf( stderr, "-----TwoTables::onLeftResize-------\n" );
 //    fprintf( stderr, "left: %d, %d, %d, %d -- parent->w = %d\n", 
-//                     _pLeft->x(), _pLeft->y(), _pLeft->w(), _pLeft->h(), this->w() );
+//                     _pLeft->x(), _pLeft->y(), _pLeft->w(), _pLeft->h(),this->w() );
 //    fprintf( stderr, "rght: %d, %d, %d, %d\n", 
 //                     _pRight->x(), _pRight->y(), _pRight->w(), _pRight->h() );
 //    fprintf( stderr, "-----------------------------------\n" );
@@ -105,7 +104,19 @@ void TwoTables::onLeftSelection( Fl_Table_Copy::TableContext context,
 //    fprintf( stderr, "rght: %d, %d, %d, %d\n", 
 //                     _pRight->x(), _pRight->y(), _pRight->w(), _pRight->h() );
  }
- 
+
+ /**causes _pLeft and _pRight to not show any more data*/
+ void TwoTables::clearData() {
+	 _pLeft->releaseTableData();
+	 _pRight->releaseTableData();
+ }
+
+ void TwoTables::addRowHeaderPopupItem( const char *pLabel, int shortcut, int
+ flags,
+										PopupMenuCallback cb, int id, void *pUserdata )
+ {
+	 _pLeft->addRowHeaderPopupItem( pLabel, shortcut, flags, cb, id, pUserdata );
+ }
+
 TwoTables::~TwoTables( ) {
 }
-
